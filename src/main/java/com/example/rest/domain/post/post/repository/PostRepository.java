70e85a3c035136137fcb7440efe1a13c3a7f21e8
kozae00 <rhwogus777@gmail.com>
2025-01-24 14:23:31 +0900

    jpa, entity, service, repository, apply

---
+++ src/main/java/com/example/rest/domain/post/post/repository/PostRepository.java
@@ -0,0 +1,6 @@
+package com.example.rest.domain.post.post.repository;
+
+import com.example.rest.domain.post.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> { }
