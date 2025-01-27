0341c2ab505c2f5531f1cea8f806a4917aa05d4d
kozae00 <rhwogus777@gmail.com>
2025-01-27 09:40:59 +0900

    RsData

--- src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
@@ -2,14 +2,13 @@ package com.example.rest.domain.post.post.controller;
 
 import com.example.rest.domain.post.post.entity.Post;
 import com.example.rest.domain.post.post.service.PostService;
+import com.example.rest.global.dto.RsData;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -23,15 +22,14 @@ public class ApiV1PostController {
     }
 
     @DeleteMapping("/{id}")
-    public Map<String, Object> delete(@PathVariable long id) {
+    public RsData delete(@PathVariable long id) {
         Post post = postService.getItem(id).get();
         postService.delete(post);
 
-        Map<String, Object> rsData = new HashMap<>();
-        rsData.put("code", "200-1");
-        rsData.put("msg", "%d번 글 삭제가 완료되었습니다.".formatted(id));
-
-        return rsData;
+        return new RsData(
+                "200",
+                "%번 글 삭제 되었습니다.".formatted(id)
+        );
     }
 
     @AllArgsConstructor
@@ -42,14 +40,13 @@ public class ApiV1PostController {
     }
 
     @PutMapping("{id}")
-    public Map<String, Object> modify(@PathVariable long id, @RequestBody ModifyForm form) {
+    public RsData modify(@PathVariable long id, @RequestBody ModifyForm form) {
         Post post = postService.getItem(id).get();
         postService.modify(post, form.getTitle(), form.getContent());
 
-        Map<String, Object> rsData = new HashMap<>();
-        rsData.put("code", "200-1");
-        rsData.put("msg", "%d번 글 수정이 완료되었습니다.".formatted(id));
-
-        return rsData;
+        return new RsData(
+                "200",
+                "%번 글 수정이 되었습니다.".formatted(id)
+        );
     }
 }
