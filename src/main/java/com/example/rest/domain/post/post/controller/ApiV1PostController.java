b2a6f769d9bdd91eaf83cb4f03d0e64fe3f9458b
kozae00 <rhwogus777@gmail.com>
2025-01-27 14:41:53 +0900

    ResponseEntity apply

--- src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
@@ -8,6 +8,8 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -85,16 +87,21 @@ public class ApiV1PostController {
     }
 
     @PostMapping
-    public RsData<WriteResBody> write(@RequestBody @Valid WriteReqBody body) {
+    public ResponseEntity<RsData<WriteResBody>> write(@RequestBody @Valid WriteReqBody body) {
         Post post = postService.write(body.title(), body.content());
 
-        return new RsData<>(
-                "200-1",
-                "글 작성이 완료되었습니다.",
-                new WriteResBody(
-                        post.getId(),
-                        postService.count())
-        );
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(
+                        new RsData<>(
+                        "200-1",
+                        "글 작성이 완료되었습니다.",
+                        new WriteResBody(
+                                post.getId(),
+                                postService.count())
+                        )
+                );
+
     }
 
 }
