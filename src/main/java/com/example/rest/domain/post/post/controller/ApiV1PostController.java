13ecbd7ed8dd3b52ea7d8b746871729a3d916766
kozae00 <rhwogus777@gmail.com>
2025-01-27 15:31:28 +0900

    response aspect apply

--- src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
@@ -67,19 +67,16 @@ public class ApiV1PostController {
     }
 
     @PutMapping("{id}")
-    public ResponseEntity modify(@PathVariable long id, @RequestBody @Valid ModifyReqBody body) {
+    public RsData<Void> modify(@PathVariable long id, @RequestBody @Valid ModifyReqBody body) {
 
         Post post = postService.getItem(id).get();
         postService.modify(post, body.title(), body.content());
 
-        return ResponseEntity
-                .noContent()
-                .build();
-//        return new RsData<>(
-//                "200-1",
-//                "%d번 글 수정이 완료되었습니다.".formatted(id),
-//                null
-//        );
+        return new RsData<>(
+                "200-1",
+                "%d번 글 수정이 완료되었습니다.".formatted(id),
+                null
+        );
     }
 
 
