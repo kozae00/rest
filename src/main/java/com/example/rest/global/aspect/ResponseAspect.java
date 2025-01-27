c8e5652756e66014adc3aefaf2755022a5889b6a
kozae00 <rhwogus777@gmail.com>
2025-01-27 15:41:57 +0900

    aop test

--- src/main/java/com/example/rest/global/aspect/ResponseAspect.java
+++ src/main/java/com/example/rest/global/aspect/ResponseAspect.java
@@ -1,5 +1,6 @@
 package com.example.rest.global.aspect;
 
+import com.example.rest.global.dto.RsData;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -29,9 +30,16 @@ public class ResponseAspect {
             @annotation(org.springframework.web.bind.annotation.ResponseBody)
             """)
     public Object test(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("pre");
-        joinPoint.proceed();
+        System.out.println("pre"); // 전처리
+
+        Object rst = joinPoint.proceed(); // 실제 수행 메서드
+
+        if(rst instanceof RsData rsData) {
+            String msg = rsData.getMsg();
+            System.out.println("msg :" + msg);
+        }
+
         System.out.println("post");
-        return null;
+        return rst;
     }
 }
