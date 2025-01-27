d267a9f730d648090b841e592f44048a6b113e75
kozae00 <rhwogus777@gmail.com>
2025-01-27 09:26:22 +0900

    modify

--- src/main/java/com/example/rest/domain/post/post/entity/Post.java
+++ src/main/java/com/example/rest/domain/post/post/entity/Post.java
@@ -2,16 +2,14 @@ package com.example.rest.domain.post.post.entity;
 
 import com.example.rest.global.BaseTime;
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Builder
+@Setter
 public class Post extends BaseTime {
     private String title;
     private String content;
