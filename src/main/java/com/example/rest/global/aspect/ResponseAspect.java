b4701451068bed85ab4f01a0853e61bf3361d115
kozae00 <rhwogus777@gmail.com>
2025-01-31 09:33:32 +0900

    response add accept

--- src/main/java/com/example/rest/global/aspect/ResponseAspect.java
+++ src/main/java/com/example/rest/global/aspect/ResponseAspect.java
@@ -1,6 +1,8 @@
 package com.example.rest.global.aspect;
 
 import com.example.rest.global.dto.RsData;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -8,7 +10,11 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@RequiredArgsConstructor
 public class ResponseAspect {
+
+    private final HttpServletResponse response;
+
     @Around("""
             (
                 within
@@ -30,16 +36,15 @@ public class ResponseAspect {
             @annotation(org.springframework.web.bind.annotation.ResponseBody)
             """)
     public Object test(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("pre"); // 전처리
-
         Object rst = joinPoint.proceed(); // 실제 수행 메서드
 
         if(rst instanceof RsData rsData) {
             String msg = rsData.getMsg();
-            System.out.println("msg :" + msg);
+
+            // 응답 코드 설정
+            response.setStatus(201);
         }
 
-        System.out.println("post");
         return rst;
     }
 }
