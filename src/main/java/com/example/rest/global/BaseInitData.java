01c52bf71f80c7fed777548768bfb8b901372ffd
kozae00 <rhwogus777@gmail.com>
2025-01-31 12:14:30 +0900

    member domain

--- src/main/java/com/example/rest/global/BaseInitData.java
+++ src/main/java/com/example/rest/global/BaseInitData.java
@@ -1,5 +1,6 @@
 package com.example.rest.global;
 
+import com.example.rest.domain.member.member.service.MemberService;
 import com.example.rest.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,16 +19,32 @@ public class BaseInitData {
     @Autowired
     @Lazy
     private BaseInitData self;
+    private final MemberService memberService;
 
     @Bean
     public ApplicationRunner applicationRunner() {
         return args-> {
-            self.init();
+            self.postInit();
+            self.memberInit();
         };
     }
 
     @Transactional
-    public void init() {
+    public void memberInit() {
+        if(memberService.count() > 0) {
+            return;
+        }
+        // 회원 샘플데이터 생성
+        memberService.join("system", "1234", "시스템");
+        memberService.join("admin", "1234", "관리자");
+        memberService.join("user1", "1234", "유저1");
+        memberService.join("user2", "1234", "유저2");
+        memberService.join("user3", "1234", "유저3");
+    }
+
+
+    @Transactional
+    public void postInit() {
         if(postService.count() > 0) {
             return;
         }
